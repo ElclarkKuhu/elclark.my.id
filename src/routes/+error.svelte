d251e92e9ceae704b1a06a8e6174a36e2664e31a
Elclark <mail@elclark.my.id>
2022-12-22 02:34:11 +0800

    :tada: SvelteKit v1.0

---
+++ src/routes/+error.svelte
@@ -0,0 +1,32 @@
+<div>
+	<article>
+		<h1>Temporary Downtime</h1>
+		<p>
+			We are currently in the process of rebuilding our website using SvelteKit 1.0. In order to
+			complete this process, we will need to take down the old website temporarily. We apologize for
+			any inconvenience this may cause and thank you for your patience while we work on improving
+			our online presence. We look forward to sharing the new and improved website with you as soon
+			as it is ready.
+		</p>
+	</article>
+</div>
+
+<style>
+	div {
+		width: 100%;
+		height: 90vh;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	article {
+		max-width: 32rem;
+		margin: 1.5rem;
+	}
+
+	h1 {
+		color: rgb(255, 179, 179);
+	}
+</style>
