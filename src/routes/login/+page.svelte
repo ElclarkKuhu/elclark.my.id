ebbeee45c5b5728f0514f9a3bfd7cf4dc8b2af90
Elclark <mail@elclark.my.id>
2023-03-09 06:40:15 +0800

    testing api

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<form
+	action="https://api.elclark.my.id/v1/auth/login?redirect=http://elclark.my.id/login"
+	method="POST"
+>
+	<input type="text" name="username" />
+	<input type="password" name="password" />
+	<button type="submit">Login</button>
+</form>
