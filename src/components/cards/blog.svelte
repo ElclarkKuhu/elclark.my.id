3de256013b5e4084e4cd9461cd22e55df7a113ac
Elclark <mail@elclark.my.id>
2023-03-25 19:57:09 +0800

    add dynmic relative time

--- src/components/cards/blog.svelte
+++ src/components/cards/blog.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import dayjs from 'dayjs';
+	import { onDestroy } from 'svelte';
 
 	export let href: string;
 	export let image: string | undefined = undefined;
@@ -8,13 +8,53 @@
 	export let date: string;
 	export let blogPath = '/blog';
 
-	function formatDate(date: string) {
-		if (dayjs(date).format('YYYY') === dayjs().format('YYYY')) {
-			return dayjs(date).format('MMMM D');
-		}
+	let dateStr: string = formatDate(date);
 
-		return dayjs(date).format('MMMM D, YYYY');
+	const interval = setInterval(() => {
+		dateStr = formatDate(date);
+	}, 1000);
+
+	function formatDate(date: string) {
+		const now = new Date();
+		const then = new Date(date);
+
+		const diff = now.getTime() - then.getTime();
+
+		const seconds = Math.floor(diff / 1000);
+		const minutes = Math.floor(seconds / 60);
+		const hours = Math.floor(minutes / 60);
+		const days = Math.floor(hours / 24);
+		const weeks = Math.floor(days / 7);
+		const months = Math.floor(weeks / 4);
+		const years = Math.floor(months / 12);
+
+		if (seconds < 60) {
+			if (seconds === 1) return 'Just now';
+			return `${seconds} seconds ago`;
+		} else if (minutes < 60) {
+			if (minutes === 1) return 'a minute ago';
+			return `${minutes} minutes ago`;
+		} else if (hours < 24) {
+			if (hours === 1) return 'an hour ago';
+			return `${hours} hours ago`;
+		} else if (days < 7) {
+			if (days === 1) return 'Yesterday';
+			return `${days} days ago`;
+		} else if (weeks < 4) {
+			if (weeks === 1) return 'a week ago';
+			return `${weeks} weeks ago`;
+		} else if (months < 12) {
+			if (months === 1) return 'a month ago';
+			return `${months} months ago`;
+		} else {
+			if (years === 1) return 'a year ago';
+			return `${years} years ago`;
+		}
 	}
+
+	onDestroy(() => {
+		clearInterval(interval);
+	});
 </script>
 
 <a href={`${blogPath}/${href}`} class:a-with-image={image}>
@@ -22,7 +62,7 @@
 
 	<div class="pill">
 		<span>{author}</span> &bullet;
-		{formatDate(date)}
+		<time datetime={date}>{dateStr}</time>
 	</div>
 
 	{#if image}
