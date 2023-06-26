3188ca0a3b052dc51fb164e8700662242e1f237f
Elclark <mail@elclark.my.id>
2023-06-27 03:00:28 +0800

    setting up for v4.5.0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,60 +1,6 @@
-<script lang="ts">
-	import Meta from '$lib/components/meta.svelte';
-	import Header from '$lib/components/header.svelte';
-	import About from '$lib/components/cards/about.svelte';
-	import Posts from '$lib/components/posts.svelte';
-	import Footer from '$lib/components/footer.svelte';
-
-	export let data;
-</script>
-
-<article>
-	<Meta
-		image="https://elclark.my.id/images/elclark.png"
-		keywords={['landing', 'home', 'homepage']}
-	/>
-
-	<Header />
-	<About />
-	<Posts posts={data.blogs} />
-
-	{#if data.blogs.length > 4}
-		<a href="/post">More Posts</a>
-	{/if}
-
-	<Footer />
-</article>
-
-<style>
-	article {
-		max-width: 480px;
-		margin: 0 auto;
-	}
-
-	a {
-		display: block;
-		margin: 0 auto;
-
-		text-align: center;
-
-		padding: 1.5rem;
-		border-radius: 1.5rem;
-
-		width: 100%;
-
-		font-size: 1.1rem;
-		font-weight: 400;
-		margin: 0.5rem auto;
-
-		color: currentColor;
-		background-color: var(--color-surface-1);
-	}
-
-	a:hover {
-		background-color: var(--color-surface-3);
-	}
-
-	a:active {
-		background-color: var(--color-surface-2);
-	}
-</style>
+<h1>Elclark</h1>
+<p>
+	Elclark is a web app that allows users to create and share their own custom flashcards. Users can
+	create decks of flashcards, add cards to those decks, and then share those decks with other users.
+	Users can also browse decks created by other users and add those decks to their own collection.
+</p>
