31c103c13082c36102fa2626d4f7618f6cdf15b7
Elclark <mail@elclark.my.id>
2023-06-30 03:16:32 +0800

    front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
-<h1>Elclark</h1>
-<p>
-	Elclark is a web app that allows users to create and share their own custom flashcards. Users can
-	create decks of flashcards, add cards to those decks, and then share those decks with other users.
-	Users can also browse decks created by other users and add those decks to their own collection.
-</p>
+<script lang="ts">
+	import Meta from '$lib/components/meta.svelte';
+
+	import Header from './header.svelte';
+	import Card from './about.svelte';
+	import Footer from '$lib/components/footer.svelte';
+</script>
+
+<Meta
+	image="https://static.elclark.my.id/image/elclark.webp"
+	keywords={['landing', 'home', 'homepage']}
+/>
+
+<Header />
+
+<main class="container">
+	<Card />
+</main>
+
+<Footer />
