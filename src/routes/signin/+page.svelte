21974b4b9c74383dd162ef21626c5436c761460b
Elclark <mail@elclark.my.id>
2023-07-03 08:19:54 +0800

    Sign in & Sign up UI

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -6,17 +6,178 @@
 	let password = ''
 </script>
 
-<main>
-	<h1>Sign In</h1>
-	{#if form?.error}
-		<p>{form.error}</p>
-	{/if}
-	<form method="POST">
-		<label for="identifier">Username</label>
-		<input type="text" name="identifier" bind:value={username} required />
-		<label for="password">Password</label>
-		<input type="password" name="password" id="password" bind:value={password} required />
-		<button type="submit">Sign In</button>
-	</form>
-	<a href="/signup">don't have an account? sign up.</a>
+<!-- TODO: Polish The UI -->
+
+<main class="container">
+	<div class="signin">
+		<a href="/" class="title-link">
+			<h2 class="title headline-large">Elclark</h2>
+		</a>
+		<span>Sing in to your account</span>
+
+		<form method="POST">
+			{#if form?.error}
+				<span class="error-message">{form.error}</span>
+			{/if}
+
+			<input
+				type="text"
+				name="identifier"
+				placeholder="Email or Username"
+				bind:value={username}
+				required
+			/>
+
+			<input
+				type="password"
+				name="password"
+				placeholder="Password"
+				bind:value={password}
+				required
+			/>
+
+			<button type="submit">Sign in</button>
+		</form>
+
+		<span class="links">
+			<a href="/signup">Register</a> /
+			<a href="/forgot-password">Forgot Password</a>
+		</span>
+	</div>
 </main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		height: 100svh;
+		padding: 0 0.5rem;
+	}
+
+	.signin {
+		width: 100%;
+		padding: 1rem;
+		max-width: 22rem;
+		padding-bottom: 1.5rem;
+
+		text-align: center;
+		border-radius: 0.5rem;
+
+		border: 0.15rem solid var(--color-surface-2);
+	}
+
+	.title {
+		color: var(--color-on-background);
+
+		background: linear-gradient(90deg, var(--color-primary) 0%, var(--color-primary) 100%);
+		background-repeat: no-repeat;
+		background-size: 0% 100%;
+		background-position: 50% 95%;
+		border-radius: 0.25rem;
+
+		transition: all 200ms ease-in-out;
+	}
+
+	.title:hover {
+		color: var(--color-on-primary);
+
+		padding: 0 0.5rem;
+		background-size: 100% 100%;
+	}
+
+	.title-link {
+		display: block;
+		text-decoration: none;
+
+		width: max-content;
+		margin: 0 auto;
+	}
+
+	.error-message {
+		display: block;
+
+		font-weight: 400;
+
+		margin: 0.25rem 0;
+		padding: 0.5rem;
+
+		color: var(--color-on-error);
+		background-color: var(--color-error);
+
+		border-radius: 0.5rem;
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+
+		margin-top: 1rem;
+		margin-bottom: 0.25rem;
+	}
+
+	h2 {
+		margin: 0;
+	}
+
+	input {
+		font: inherit;
+
+		margin: 0.25rem 0;
+		padding: 0.5rem 0.75rem;
+
+		border: 0.15rem solid var(--color-surface-2);
+		border-radius: 0.5rem;
+
+		background-color: transparent;
+
+		transition: border-color 0.2s ease-in-out;
+	}
+
+	input:focus {
+		outline: none;
+		border-color: var(--color-primary);
+	}
+
+	input:hover {
+		border-color: var(--color-primary);
+	}
+
+	input::placeholder {
+		color: var(--color-on-background);
+		opacity: 0.5;
+	}
+
+	button {
+		font: inherit;
+		font-weight: 500;
+		cursor: pointer;
+
+		margin: 0.25rem 0;
+		padding: 0.5rem 0.75rem;
+
+		border: none;
+		border-radius: 0.5rem;
+
+		color: var(--color-primary);
+		background-color: var(--color-surface-2);
+
+		transition: all 200ms ease-in-out;
+	}
+
+	button:hover {
+		color: var(--color-on-primary-container);
+		background-color: var(--color-primary-container);
+	}
+
+	a {
+		color: inherit;
+	}
+
+	.links {
+		font-size: 0.9rem;
+		font-weight: 400;
+	}
+</style>
